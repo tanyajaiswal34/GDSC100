aac663bfbe79158b218bacfe6efd0e541920606a
tanyajaiswal34 <134253246+tanyajaiswal34@users.noreply.github.com>
2024-01-20 19:44:27 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,15 @@
+import streamlit as st
+st.title('demo App')
+st.write('This is a demo app')
+with st.sidebar:
+  st.write('This is a sidebar')
+  col1,col2 = st.columns(2)
+  with col1:
+        a = st.number_input('Enter a Value',value=10)
+    with col2:
+         b = st.text_input('Enter a text')
+sub = st.button(label='Submit')
+is sub:
+st.write(a)
+st.write(b)
+print(a)
