f504da8a9b0cce9fe960b49b5bb129ab13618154
tanyajaiswal34 <134253246+tanyajaiswal34@users.noreply.github.com>
2024-01-20 19:52:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,10 +4,10 @@ st.write('This is a demo app')
 with st.sidebar:
   st.write('This is a sidebar')
   col1,col2 = st.columns(2)
-  with col1:
-        a = st.number_input('Enter a Value',value=10)
-    with col2:
-         b = st.text_input('Enter a text')
+with col1:
+  a = st.number_input('Enter a Value',value=10)
+with col2:
+  b = st.text_input('Enter a text')
 sub = st.button(label='Submit')
 is sub:
 st.write(a)
